7f8886020382cf8c6ee786e6dd4fa7ecd7bf94ff
afdon <53676753+afdon@users.noreply.github.com>
2023-05-30 19:37:36 -0400

    added box

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,3 +1,10 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+import { useMemo } from 'react';
+import { HiHome } from 'react-icons/hi';
+import { BiSearch } from 'react-icons/bi';
+import Box from './Box';
 
 
 interface SidebarProps {
@@ -7,10 +14,39 @@ interface SidebarProps {
 const Sidebar: React.FC<SidebarProps> = ({
     children
 }) => {
+    const pathname = usePathname();
+
+    const routes = useMemo(() => [
+        {
+            icon: HiHome,
+            label: 'Home',
+            active: pathname !== '/search',
+            href: '/',
+        },
+        {
+            icon: BiSearch,
+            label: 'Search',
+            active: pathname === '/search',
+            href: '/search'
+        }
+        
+    ], [pathname])
     
     return (
-        <div>
-            {children}
+        <div className='flex h-full'>
+            <div className='
+            hidden
+            md:flex
+            flex-col
+            gap-y-2
+            h-full
+            w-[300px]
+            p-2
+            '>
+                <Box>
+                    Sidebar Navigation
+                </Box>
+            </div>
         </div>
     );
 }
