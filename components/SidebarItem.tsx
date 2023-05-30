049c427caccb35a7be0a3c3cd1dff2e7b28d1678
afdon <53676753+afdon@users.noreply.github.com>
2023-05-30 19:47:01 -0400

    added interface for SidebarItem

--- components/SidebarItem.tsx
+++ components/SidebarItem.tsx
@@ -1,13 +1,23 @@
+import { IconType } from "react-icons";
 
+interface SidebarItemProps {
+    icon: IconType;
+    label: string;
+    active?: boolean;
+    href: string;
+}
 
-
-
-const SidebarItem = () => {
-  return (
-    <div>
-        SidebarItem
+const SidebarItem: React.FC<SidebarItemProps> = ({
+    icon,
+    label,
+    active,
+    href
+}) => {
+    return (
+        <div>
+            SidebarItem
         </div>
-  )
+    )
 }
 
 export default SidebarItem;
