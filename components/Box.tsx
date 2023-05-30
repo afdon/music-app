e4b6d72326d6f28a03f49761dfd6ec9b5fd5d121
afdon <53676753+afdon@users.noreply.github.com>
2023-05-30 19:41:33 -0400

    installed twMerge & used in Box

--- components/Box.tsx
+++ components/Box.tsx
@@ -1,6 +1,6 @@
 
 
-
+import { twMerge } from 'tailwind-merge';
 
 interface BoxProps {
     children: React.ReactNode;
@@ -12,6 +12,17 @@ const Box: React.FC<BoxProps> = ({
     className
 }) => {
     
-    return <div>{children}</div>
+    return <div
+    className={twMerge(`
+    bg-neutral-900
+    rounded-lg
+    h-fit
+    w-full
+    `,
+    className
+    )}
+    >
+        {children}
+        </div>
 }
 export default Box;
