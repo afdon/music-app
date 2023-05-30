ddf1646cdd08b65d96f96982f1103cdda8d6e2b8
afdon <53676753+afdon@users.noreply.github.com>
2023-05-30 19:45:13 -0400

    added sidebarItem

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,6 +5,7 @@ import { useMemo } from 'react';
 import { HiHome } from 'react-icons/hi';
 import { BiSearch } from 'react-icons/bi';
 import Box from './Box';
+import SidebarItem from './SidebarItem';
 
 
 interface SidebarProps {
@@ -44,7 +45,25 @@ const Sidebar: React.FC<SidebarProps> = ({
             p-2
             '>
                 <Box>
-                    Sidebar Navigation
+                    <div
+                    className='
+                    flex
+                    flex-col
+                    gap-y-4
+                    px-5
+                    py-4
+                    '
+                    >
+                        {routes.map((item) => (
+                            <SidebarItem
+                            key={item.label}
+                            {...item}
+                            />
+                        ))}
+                    </div>
+                </Box>
+                <Box className='overflow-y-auto h-full'>
+                    Song Library
                 </Box>
             </div>
         </div>
---
+++ components/SidebarItem.tsx
@@ -0,0 +1,13 @@
+
+
+
+
+const SidebarItem = () => {
+  return (
+    <div>
+        SidebarItem
+        </div>
+  )
+}
+
+export default SidebarItem;
