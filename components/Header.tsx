77bac31466f91a072fbde8ec16231e4c8cce26e2
afdon <53676753+afdon@users.noreply.github.com>
2023-05-30 20:40:51 -0400

    modified signup button & created login button

--- components/Header.tsx
+++ components/Header.tsx
@@ -111,8 +111,27 @@ const Header: React.FC<HeaderProps> = ({
                 ">
                     <>
                         <div>
-                            <Button>
+                            <Button
+                                onClick={() => { }}
+                                className="
+                            bg-transparent
+                            text-neutral-300
+                            font-medium
+                            "
+                            >
                                 Sign up
+                            </Button>
+                        </div>
+                        <div>
+                            <Button
+                                onClick={() => { }}
+                                className="
+                            bg-white
+                            px-6
+                            py-2
+                            "
+                            >
+                                Log in
                             </Button>
                         </div>
                     </>
