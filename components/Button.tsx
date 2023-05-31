c45e8d371b7dffdb40e7f81836dd07d86ee21f5e
afdon <53676753+afdon@users.noreply.github.com>
2023-05-30 20:29:53 -0400

    created Button.tsx

---
+++ components/Button.tsx
@@ -0,0 +1,16 @@
+
+type ButtonProps = {
+    
+};
+
+const Button: React.FC<ButtonProps> = ({
+
+}) => {
+    
+    return (
+        <div>
+            button
+        </div>
+    )
+}
+export default Button;
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,7 @@ import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
 
 interface HeaderProps {
     children: React.ReactNode;
@@ -101,6 +102,20 @@ const Header: React.FC<HeaderProps> = ({
                         ">
                         <BiSearch className="text-black" size={20} />
                     </button>
+                </div>
+                <div className="
+                flex
+                justify-between
+                items-center
+                gap-x-4
+                ">
+                    <>
+                        <div>
+                            <Button>
+
+                            </Button>
+                        </div>
+                    </>
                 </div>
             </div>
         </div>
