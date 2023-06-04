89914c0c070537821e78ae69fc32ace554de0aa5
afdon <53676753+afdon@users.noreply.github.com>
2023-06-04 18:13:22 -0400

    finished useUser

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -1,6 +1,6 @@
 import { User } from "@supabase/auth-helpers-nextjs";
 import { useSessionContext, useUser as useSupaUser } from "@supabase/auth-helpers-react";
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 import { Subscription, UserDetails } from "@/types";
 
@@ -76,3 +76,12 @@ export const MyUserContextProvider = (props: Props) => {
 
     return <MyUserContextProvider value={value} {...props} />
 };
+
+export const useUser = () => {
+    const context = useContext(UserContext);
+    if (context === undefined) {
+        throw new Error('useUser must be used within a MyUserContextProvider');
+    }
+
+    return context;
+}
