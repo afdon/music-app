af1918b6d659e2c5d4f04c1dba2bd9fd334cfdf5
afdon <53676753+afdon@users.noreply.github.com>
2023-06-04 23:40:49 -0400

    uploadModal works - open and close on click.

--- components/Library.tsx
+++ components/Library.tsx
@@ -5,16 +5,21 @@ import { AiOutlinePlus } from 'react-icons/ai';
 
 import useAuthModal from '@/hooks/UseAuthModal';
 import { useUser } from '@/hooks/useUser';
+import useUploadModal from '@/hooks/useUploadModal';
 
 const Library = () => {
     const authModal = useAuthModal();
+    const uploadModal = useUploadModal();
     const { user } = useUser();
 
     const onClick = () => {
         if (!user) {
             return authModal.onOpen();
         }
-        // handle upload later
+
+        // todo: check for subscription
+
+        return uploadModal.onOpen();
     };
 
     return (
