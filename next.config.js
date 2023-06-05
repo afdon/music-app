348de614309dde490be51deeaec64bed8cecb500
afdon <53676753+afdon@users.noreply.github.com>
2023-06-05 01:15:28 -0400

    added song images to page; configured nextconfig to accept images coming from my supabase domain

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            "mwcjoxkbbputgmsfygav.supabase.co" // supabase reference ID
+        ]
+    }
+}
 
 module.exports = nextConfig
