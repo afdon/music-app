9508c0c0fd21284fdc538c03aad189cf998d9c3a
afdon <53676753+afdon@users.noreply.github.com>
2023-06-05 23:30:04 -0400

    added player to media item

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import useLoadImage from "@/hooks/useLoadImage";
+import usePlayer from "@/hooks/usePlayer";
 import { Song } from "@/types";
 
 import Image from "next/image";
@@ -14,14 +15,15 @@ const MediaItem: React.FC<MediaItemProps> = ({
     data,
     onClick
 }) => {
+    const player = usePlayer();
     const imageURL = useLoadImage(data);
 
     const handleClick = () => {
         if (onClick) {
             return onClick(data.id);
         }
-
-        // todo: default turn on player
+        
+        return player.setId(data.id);
     }
 
     return (
