6c1028101217c12fddeff43b22321620364ece9d
afdon <53676753+afdon@users.noreply.github.com>
2023-06-05 22:58:23 -0400

    using subscribe modal in useOnPlay so that only subscribed users can play

--- hooks/useOnPlay.ts
+++ hooks/useOnPlay.ts
@@ -1,18 +1,25 @@
 import { Song } from "@/types"
 
+import { useUser } from "./useUser";
 import usePlayer from "./usePlayer"
 import useAuthModal from "./UseAuthModal";
-import { useUser } from "./useUser";
+import useSubscribeModal from "./useSubscribeModal";
 
 
 const useOnPlay = (songs: Song[]) => {
     const player = usePlayer();
     const authModal = useAuthModal();
-    const { user } = useUser();
+    const subscribeModal = useSubscribeModal();
+    const { user, subscription } = useUser();
 
     const onPlay = (id: string) => {
         if (!user) {
             return authModal.onOpen();
+        }
+
+        // only subscribed users can play songs
+        if (!subscription) {
+            return subscribeModal.onOpen();
         }
 
         player.setId(id);
