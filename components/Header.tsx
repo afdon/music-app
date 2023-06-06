cb64bc41399ad4f97df13a23118b0a76a578f939
afdon <53676753+afdon@users.noreply.github.com>
2023-06-06 00:15:16 -0400

    styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -48,7 +48,7 @@ const Header: React.FC<HeaderProps> = ({
             className={twMerge(`
         h-fit
         bg-gradient-to-b
-        from-emerald-800
+        from-orange-600
         p-6
         `,
                 className
